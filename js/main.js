35bd505a30c80f4c23316bcc63c83549ce4a60b1
Under_Koen <koenvanstaveren@hotmail.com>
2017-11-22 13:44:23 +0100

    slideshow
    Signed-off-by: Under_Koen <koenvanstaveren@hotmail.com>

---
+++ js/main.js
@@ -0,0 +1,29 @@
+function nextSlideShow(t) {
+    var items = t.parentElement.getElementsByClassName("items")[0].getElementsByClassName("item");
+    for (i = 0; i < items.length; i++) {
+        var element = items[i];
+        if (element.classList.contains("active")) {
+            i = i + 1;
+            if (i === items.length) i = 0;
+            var next = items[i];
+            element.classList.remove("active");
+            next.classList.add("active");
+            break
+        }
+    }
+}
+
+function previousSlideShow(t) {
+    var items = t.parentElement.getElementsByClassName("items")[0].getElementsByClassName("item");
+    for (i = 0; i < items.length; i++) {
+        var element = items[i];
+        if (element.classList.contains("active")) {
+            i = i - 1;
+            if (i === -1) i = items.length-1;
+            var next = items[i];
+            element.classList.remove("active");
+            next.classList.add("active");
+            break
+        }
+    }
+}
